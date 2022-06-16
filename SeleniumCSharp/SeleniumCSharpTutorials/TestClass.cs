d22a930ce6f17f36702dd05a9d1c6758abe85753
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-06-16 13:25:57 +0200

    wyrzucenie wspólnych elementów z testów do klasy basetest

--- SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
@@ -5,20 +5,19 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumCSharpTutorials.BaseClass;
 
 namespace SeleniumCSharpTutorials
 {
     [TestFixture]
-    public class TestClass
+    public class TestClass : BaseTest
     {
         [Test]
         public void TestMethod()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.facebook.com";
+            
             IWebElement emailTxtField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTxtField.SendKeys("Selenium C#");
-            driver.Quit();
         }
     }
 }
