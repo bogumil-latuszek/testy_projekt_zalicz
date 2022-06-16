dce372748066cb85d72dbc95bc2a51f3be87b106
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-06-16 23:29:47 +0200

    do testu: zatwierdzenie ciasteczek i otworzenie okna rejestracji

--- SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using SeleniumCSharpTutorials.BaseClass;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumCSharpTutorials
 {
@@ -18,8 +19,24 @@ namespace SeleniumCSharpTutorials
         {
             IWebElement emailTxtField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTxtField.SendKeys("Selenium C#");
+
+
+            IWebElement cookieButton = driver.FindElement(By.XPath(".//button[@title='Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie']"));
+            cookieButton.Click();
+            Thread.Sleep(1000);
+
+            IWebElement regButton = driver.FindElement(By.XPath("//a[@role='button'][@data-testid='open-registration-form-button']"));//[@ajaxify=' / reg / spotlight / ']
+            regButton.Click();
+            Thread.Sleep(5000);
+
+            //IWebElement monthDropdownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            //SelectElement element = new SelectElement(monthDropdownList);
+            //element.SelectByIndex(1);
+            //element.SelectByText("Mar");
+            //element.SelectByValue("6");
         }
 
+        /*
         [Test, Category("Regression Testing")]
         public void TestMethod2()
         {
@@ -32,7 +49,8 @@ namespace SeleniumCSharpTutorials
         {
             IWebElement emailTxtField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTxtField.SendKeys("Selenium C#");
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
         }
+        */
     }
 }
